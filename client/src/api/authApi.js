0ee44fdde08a61d11dc146464aa62604bd498617
Nethmi Kaumudi <118107008+NethmiKaumudi@users.noreply.github.com>
2024-06-12 15:09:22 +0530

    update nackend product crud

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -50,26 +50,4 @@ export const loginUser = async (username, password) => {
 };
 
 
-const BASE_URL = "http://localhost:4000"; // Replace this with your backend URL
-
-export const addProduct = async (productData) => {
-  try {
-    const response = await fetch(`${BASE_URL}/products/add`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-      body: JSON.stringify(productData),
-    });
-
-    if (!response.ok) {
-      throw new Error("Failed to add product");
-    }
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    throw new Error("Failed to add product");
-  }
-};
 
--- client/src/pages/AdminProductAddPage.tsx
+++ client/src/pages/AdminProductAddPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { addProduct } from "../api/authApi";
+import { addProduct } from "../api/productAoi";
 import Swal from "sweetalert2";
 import { IoClose } from "react-icons/io5";
 
@@ -56,11 +56,17 @@ const AddProduct: React.FC<AddProductPopupProps> = ({ onClose }) => {
     }
   };
   return (
-    <div className="flex flex-col items-center justify-center bg-primaryDark rounded-lg shadow-lg p-8 relative">
-      <h2 className="text-xl font-semibold mb-4 text-white">Add Product</h2>
-      <form onSubmit={handleSubmit} className="bg-white p-8 rounded-lg shadow-lg w-full">
+    <div className="flex flex-col items-center justify-center bg-primaryDark rounded-lg shadow-lg p-4 relative w-full max-w-xl"> {/* Increased max width to max-w-xl */}
+    <h2 className="text-xl font-semibold mb-2 text-white">Add Product</h2>
+    <form
+      onSubmit={handleSubmit}
+      className="bg-white p-6 rounded-lg shadow-lg w-full"
+    >
         <div className="mb-6">
-          <label htmlFor="ProductName" className="block text-gray-700 text-sm font-bold mb-2">
+          <label
+            htmlFor="ProductName"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
             Product Name
           </label>
           <input
@@ -69,12 +75,15 @@ const AddProduct: React.FC<AddProductPopupProps> = ({ onClose }) => {
             value={product.name}
             onChange={(e) => handleChange("name", e.target.value)}
             required
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           />
         </div>
-  
+
         <div className="mb-6">
-          <label htmlFor="ProductDescription" className="block text-gray-700 text-sm font-bold mb-2">
+          <label
+            htmlFor="ProductDescription"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
             Product Description
           </label>
           <input
@@ -83,12 +92,15 @@ const AddProduct: React.FC<AddProductPopupProps> = ({ onClose }) => {
             value={product.description}
             onChange={(e) => handleChange("description", e.target.value)}
             required
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           />
         </div>
-  
+
         <div className="mb-6">
-          <label htmlFor="ProductPrice" className="block text-gray-700 text-sm font-bold mb-2">
+          <label
+            htmlFor="ProductPrice"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
             Product Price
           </label>
           <input
@@ -97,11 +109,14 @@ const AddProduct: React.FC<AddProductPopupProps> = ({ onClose }) => {
             value={product.price}
             onChange={(e) => handleChange("price", e.target.value)}
             required
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           />
         </div>
         <div className="mb-6">
-          <label htmlFor="category" className="block text-gray-700 text-sm font-bold mb-2">
+          <label
+            htmlFor="category"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
             Category
           </label>
           <input
@@ -110,11 +125,11 @@ const AddProduct: React.FC<AddProductPopupProps> = ({ onClose }) => {
             value={product.category}
             onChange={(e) => handleChange("category", e.target.value)}
             required
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           />
         </div>
-        <div className="mb-6">
-          <label htmlFor="image" className="block text-gray-700 text-sm font-bold mb-2">
+        <div className="mb-6 flex items-center">
+          <label htmlFor="image" className="block text-gray-700 text-sm font-bold mr-2">
             Product Image
           </label>
           <input
@@ -127,17 +142,19 @@ const AddProduct: React.FC<AddProductPopupProps> = ({ onClose }) => {
               }
             }}
             required
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-2"
           />
           {product.image && (
-            <div>
-              <img src={URL.createObjectURL(product.image)} alt="Product" className="mt-2 w-32 h-auto" />
-            </div>
+            <img src={URL.createObjectURL(product.image)} alt="Product" className="w-32 h-auto" />
           )}
         </div>
-  
+
+
         <div className="mb-6">
-          <label htmlFor="ProductQuantity" className="block text-gray-700 text-sm font-bold mb-2">
+          <label
+            htmlFor="ProductQuantity"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
             Product Quantity
           </label>
           <input
@@ -146,10 +163,10 @@ const AddProduct: React.FC<AddProductPopupProps> = ({ onClose }) => {
             value={product.quantity}
             onChange={(e) => handleChange("quantity", e.target.value)}
             required
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           />
         </div>
-  
+
         {error && <div className="text-red-500">{error}</div>}
         <button
           type="submit"
@@ -159,10 +176,12 @@ const AddProduct: React.FC<AddProductPopupProps> = ({ onClose }) => {
           Add Product
         </button>
       </form>
-      <IoClose className="absolute top-4 right-4 text-white size-8 cursor-pointer" onClick={onClose} />
+      <IoClose
+        className="absolute top-4 right-4 text-white size-8 cursor-pointer"
+        onClick={onClose}
+      />
     </div>
   );
-  
 };
 
 export default AddProduct;
